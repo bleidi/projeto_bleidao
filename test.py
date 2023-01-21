c8c7712c87a1d429fd8f9cd98568c996438944fa
Rafael Souza Oliveira <rafael.oliveira@betha.com.br>
2023-01-20 23:12:57 -0300

    Adicionando metodo somar

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import sys
+
 str = "hello world"
 print(str)
 
@@ -5,3 +7,14 @@ y=1
 print(y)
 x = 1
 print(x)
+
+def somar(primeiro_op,segundo_operador):
+    print("Começou o método")
+    print(f"O primeiro operador é: {primeiro_op}")
+    print(f"O segundo operador é: {segundo_operador}")
+    print(primeiro_op + segundo_operador)
+    print("Terminou o método")
+
+somar(x,y)
+
+somar(2,2)
